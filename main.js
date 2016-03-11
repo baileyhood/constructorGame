c048694e175332fbd2622219fb6452a993db5e2d
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-10 19:59:28 -0500

    starting to move game to DOM

--- main.js
+++ main.js
@@ -1,142 +1,153 @@
 //FIRST CONSTRUCTOR
-function Player (options){
-  var options = options || {};
-  this.name = options.name || 'UnchosenPlayerName';
-  this.points = options.points || 0; //stops at 11
-  this.power = options.power || 1;
-  this.speed = options.speed || 1;
-  this.spin = options.spin || 1;
-  this.mentalStrength  = options.mentalStrength || 100;
-  this.rating = this.power + this.speed + this.spin + this.mentalStrength; //adding all together
-  this.shot = function (opponent) {
-    opponent.pointLoss(this);
-  };
+function Player(options) {
+        var options = options || {};
+        this.name = options.name || 'UnchosenPlayerName';
+        this.points = options.points || 0; //stops at 11
+        this.power = options.power || 1;
+        this.speed = options.speed || 1;
+        this.spin = options.spin || 1;
+        this.mentalStrength = options.mentalStrength || 100;
+        this.paddle = options.paddle;
+        this.location = options.location;
+        this.rating = this.power + this.speed + this.spin + this.mentalStrength; //adding all together
+        this.shot = function(opponent) {
+            opponent.pointLoss(this);
+        };
 
-  this.pointLoss = function (opponent) {
-    if (opponent.paddle) {
-     var totalRatingOnePaddle = opponent.rating + opponent.paddle.paddleRating; //add paddleRating to opponent's rating
-        if(totalRatingOnePaddle > this.rating) { //if paddleRating and rating is greater than other player's rating
-          opponent.points = opponent.points + 1; // give them one point
-          alert (opponent.name + " won the point! The score is: " + this.points + "|" + opponent.points);
-          console.log ("line 21");
-     }
-    }
-    else if (this.rating > opponent.rating) {
-      this.points = this.points + 1;
-      alert (this.name + " won the point! The score is: " + opponent.points + "|" + this.points);
-      console.log ("line 27");
-    }
-    else {
-      opponent.points = opponent.points + 1;
-      alert (this.name + " lost the point! The score is: " + opponent.points + "|" + this.points);
-      console.log ("line 32");
-      }
-  }; //end of pointLoss function
+        this.pointLoss = function(opponent) {
+            if (opponent.paddle) {
+                var totalRatingWithPaddle = opponent.rating + opponent.paddle.paddleRating; //add paddleRating to opponent's rating
+                if (totalRatingWithPaddle > this.rating) { //if paddleRating and rating is greater than other player's rating
+                    opponent.points = opponent.points + 1; // give them one point
+                    alert(opponent.name + " won the point! The score is: " + this.points + "|" +
+                        opponent.points);
+                    console.log("line 23");
+                }
+            } else if (this.rating > opponent.rating) {
+                this.points = this.points + 1;
+                alert(this.name + " won the point! The score is: " + opponent.points + "|" +
+                    this.points);
+                console.log("line 29");
+            } else {
+                opponent.points = opponent.points + 1;
+                alert(this.name + " lost the point! The score is: " + opponent.points + "|" +
+                    this.points);
+                console.log("line 34");
+            }
+        }; //end of pointLoss function
 
 
-  if (this.rating > 10) {
-    alert("recalculate " + this.name + " attributes so that total for 'Rating' is 10. Right now the total is " + this.rating + ".");
-  }
+        if (this.rating > 10) {
+            alert("recalculate " + this.name +
+                " attributes so that total for 'Rating' is 10. Right now the total is " + this.rating +
+                ".");
+        }
 
-this.paddleChoice = function (name, strength, spin) {
-  this.paddle = new Paddle ({
-    name: name,
-    strength: strength,
-    spin: spin});
-};
+        this.paddleChoice = function(name, strength, spin) {
+            this.paddle = name;
+        };
+        this.locationChoice = function(name) {
+            this.location = name;
+        };
 
-} //END OF FIRST CONSTRUCTOR
+    } //END OF FIRST CONSTRUCTOR
 
 //SECOND CONSTRUCTOR
-function Paddle (options) {
-  var options = options || {};
-  this.name = options.name || "Unchosen Paddle Name";
-  this.paddleStrength = options.strength || 1;
-  this.paddleSpin = options.spin || 1;
-  this.paddleRating = this.paddleStrength + this.paddleSpin;
+function Paddle(options) {
+    var options = options || {};
+    this.name = options.name || "Unchosen Paddle Name";
+    this.paddleStrength = options.strength || 1;
+    this.paddleSpin = options.spin || 1;
+    this.paddleRating = this.paddleStrength + this.paddleSpin;
 
-  if (this.paddleRating > 10) {
-    alert("recalculate '" + this.name + "' attributes so that total for 'Rating' is 10. Right now the total is " + this.paddleRating + ".");
-  }
+    if (this.paddleRating > 10) {
+        alert("recalculate '" + this.name +
+            "' attributes so that total for 'Rating' is 10. Right now the total is " + this
+            .paddleRating + ".");
+    }
 }
 
 //THIRD CONSTRUCTOR
-function Location (options) {
-  var options = options || {};
-  this.randomDistractionLevel = function () {
-    var random = Math.floor(Math.random() * 10 + 1);
-    return random + this.setDistractionLevel;
+function Location(options) {
+    var options = options || {};
+    this.randomDistractionLevel = function() {
+        var random = Math.floor(Math.random() * 10 + 1);
+        return random + this.setDistractionLevel;
     };
-  this.name = options.name;
-  this.setDistractionLevel = options.setDistractionLevel;
-  this.totalDistractionLevel = this.setDistractionLevel + this.randomDistractionLevel();
+    this.name = options.name;
+    this.setDistractionLevel = options.setDistractionLevel;
+    this.totalDistractionLevel = this.setDistractionLevel + this.randomDistractionLevel();
 }
 
 //PLAYER OPTIONS
 
-var justin = new Player ({
-  name:"Justin",
-  points: 0,
-  power: 1,
-  speed: 2,
-  spin: 2,
-  mentalStrength: 4});
+var justin = new Player({
+    name: "Justin",
+    points: 0,
+    power: 1,
+    speed: 2,
+    spin: 2,
+    mentalStrength: 4
+});
 
-var russ = new Player ({
-  name:"Russ",
-  points: 0,
-  power: 3,
-  speed: 3,
-  spin: 2,
-  mentalStrength: 2});
+var russ = new Player({
+    name: "Russ",
+    points: 0,
+    power: 3,
+    speed: 3,
+    spin: 2,
+    mentalStrength: 2
+});
 
-var nathan = new Player ({
-    name:"Nathan",
+var nathan = new Player({
+    name: "Nathan",
     points: 0,
     power: 4,
     speed: 1,
     spin: 3,
-    mentalStrength: 2});
+    mentalStrength: 2
+});
 
-var woody = new Player ({
-    name:"Woody",
+var woody = new Player({
+    name: "Woody",
     points: 0,
     power: 1,
     speed: 0,
     spin: 2,
-    mentalStrength: 6});
+    mentalStrength: 6
+});
 
 //PADDLE OPTIONS
 
-var spinner = new Paddle ({
-  name: "Spinner",
-  strength: 2,
-  spin: 7
+var spinner = new Paddle({
+    name: "Spinner",
+    strength: 2,
+    spin: 7
 });
 
-var smash = new Paddle ({
-  name: "Spinner",
-  strength: 7,
-  spin: 0
+var smash = new Paddle({
+    name: "Smasher",
+    strength: 7,
+    spin: 2
 });
 
 //LOCATION OPTIONS
-var bar = new Location ({
-  name: "Noisy Bar",
-  setDistractionLevel: 5
+var bar = new Location({
+    name: "Noisy Bar",
+    setDistractionLevel: 5
 });
 
-var office = new Location ({
-  name: "Office",
-  setDistractionLevel: 1
+var office = new Location({
+    name: "Office",
+    setDistractionLevel: 1
 });
 
-var tiy = new Location ({
-  name: "The Iron Yard",
-  setDistractionLevel: 4
+var tiy = new Location({
+    name: "The Iron Yard",
+    setDistractionLevel: 4
 });
 
-var party = new Location ({
-  name: "Loud Party",
-  setDistractionLevel: 6
+var party = new Location({
+    name: "Loud Party",
+    setDistractionLevel: 6
 });
---
+++ main2.js
@@ -0,0 +1,149 @@
+////////////////////////////
+//FIRST CONSTRUCTOR
+///////////////////////////
+
+function Player(options) {
+  var options = options || {};
+  this.name = options.name || "Richard Head";
+  this.points = 0;
+  this.energy = 10;
+  this.forehand = options.forehand;
+  this.backhand = options.backhand;
+  this.serve = options.serve;
+  this.mentalStrength = options.mentalStrength;
+  this.rating = this.forehand + this.backhand + this.serve + this.mentalStrength;
+}
+
+////////////////////////////
+//SECOND CONSTRUCTOR
+///////////////////////////
+
+function Location (options) {
+  var options = options || {};
+  this.randomDistractionLevel = function() {
+      var random = function () {return Math.round(Math.random() * (1 - 5) + 1)};
+      return random() + this.setDistractionLevel;
+  };
+  this.name = options.name;
+  this.setDistractionLevel = options.setDistractionLevel;
+  this.totalDistractionLevel = this.setDistractionLevel + this.randomDistractionLevel();
+}
+
+
+////////////////////////////
+//THIRD CONSTRUCTOR
+///////////////////////////
+
+function Paddle (options) {
+  var options = options || {};
+  this.name = options.name;
+  this.spin = options.spin;
+  this.power = options.power;
+}
+
+////////////////////////////
+//PLAYER OPTIONS
+///////////////////////////
+
+var russPlayer = new Player({
+    name: "Russ",
+    forehand: 4,
+    backhand: 2,
+    serve: 2,
+    mentalStrength: 1
+});
+
+var justinPlayer = new Player({
+    name: "Justin",
+    forehand: 3,
+    backhand: 1,
+    serve: 1,
+    mentalStrength: 4
+});
+
+var weesiePlayer = new Player({
+    name: "Weesie",
+    forehand: 3,
+    backhand: 3,
+    serve: 0,
+    mentalStrength: 3
+});
+
+var dinaPlayer = new Player({
+    name: "Dina",
+    forehand: 6,
+    backhand: 0,
+    serve: 2,
+    mentalStrength: 1
+});
+
+////////////////////////////
+//LOCATION OPTIONS
+///////////////////////////
+
+var barLocal = new Location({
+    name: "Noisy Bar",
+    setDistractionLevel: 5
+});
+
+var officeLocal = new Location({
+    name: "Office",
+    setDistractionLevel: 1
+});
+
+var tiyLocal = new Location({
+    name: "The Iron Yard",
+    setDistractionLevel: 3
+});
+
+var partyLocal = new Location({
+    name: "Loud Party",
+    setDistractionLevel: 6
+});
+
+////////////////////////////
+//PADDLE OPTIONS
+///////////////////////////
+
+
+var spinnerPaddle = new Paddle ({
+  name: "Spinner",
+  spin: 7,
+  power: 1,
+});
+
+var smashPaddle = new Paddle ({
+  name: "smash",
+  spin: 1,
+  power: 8,
+});
+
+var allRoundPaddle = new Paddle ({
+  name: "All-Rounder",
+  spin: 4,
+  power: 4,
+});
+
+
+////////////////////////////
+//BEGINNING OF GAME
+///////////////////////////
+$(document).ready(function() {
+  page.init();
+});
+
+var charChoice = [
+  russPlayer, justinPlayer, dinaPlayer, weesiePlayer
+]
+var charPull = function (){
+  charDisplay = "";
+  charChoice.forEach(function(el){
+    charDisplay += el.name + " ";
+  });
+  return charDisplay;
+};
+
+function beginGame () {
+  var playerChoice = prompt("Select your player: " + charPull());
+  console.log(playerChoice);
+}
